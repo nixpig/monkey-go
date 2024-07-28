ef8e76accd12176775007acad140c875ab27692f
nixpig <143995476+nixpig@users.noreply.github.com>
2024-07-28 16:20:36 +0100

    chore: start from scratch

--- main.go
+++ main.go
@@ -1,20 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"os/user"
-
-	"github.com/nixpig/monkey-go/repl"
-)
-
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Hello %s! This is the Monkey REPL!\n", user.Username)
-
-	repl.Start(os.Stdin, os.Stdout)
 }
